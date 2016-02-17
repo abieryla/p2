ea5ea4523b50f461ac6bc87b090e3cc8ace31903
Allyson Bieryla <abieryla@cfa.harvard.edu>
2016-02-17 16:20:39 -0500

    password generator working with add a number and add a special character features. some error checking for user input as well.

--- index.php
+++ index.php
@@ -17,36 +17,54 @@ ini_set('display_errors', 1);
 <body>
 	<h1> Password Generator </h1>
 
-	<!-- Form that user will use to input password settings -->
+
+
+<!-- Form that user will use to input password settings -->
+
 	<form action='index.php' method='GET'>
 		Enter number of words in password (0-9): 
 		<input type='text' name='pwdlength' size="5" maxlength="1"><br>
-<!--		
+		
 		Add a number? 
-		<input type='checkbox' name='option' value='number'><br>
+		<input type='checkbox' name='number'><br>
 		Add a special character?
-		<input type='checkbox' name='option' value='special'><br>
--->
+		<input type='checkbox' name='special'><br>
+
 		<input type='submit' value='Generate a password!'>
 	</form>
 
-	<!-- Generate Password  using logic.php and print to screen (default to display 3 words) -->
+
+<!-- Generate Password  using logic.php and print to screen (default to display 3 words) -->
 	
 	<table>
 	   <tr>
 	      <td>
 		   <?php $length = count($pwd); ?>
+	<!-- print out words -->
 		   <?php for($x = 1; $x <= $length; $x++): ?>
 		   	<?php if($x < $length): ?>
 		   		<?php echo $pwd[$x]; ?> -
 		   	<?php else: ?>
 		   		<?php echo $pwd[$x]; ?>
+	<!-- check for number -->
+				<?php if($num != 0): ?>
+					<?php echo $num; ?>
+				<?php endif; ?>
+	<!-- check for special character -->
+				<?php if(isset($spec)): ?>
+                                        <?php echo $spec; ?>
+                                <?php endif; ?>
+
 		   	<?php endif; ?>
 		   <?php endfor; ?>
 	      </td>
 	   </tr>
 	</table>
 
+	<!-- check for valid input and display error if necessary -->
+	<?php if(isset($error)): ?>
+		<?php echo $error; ?>
+	<?php endif; ?>
 
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -7,6 +7,9 @@ $words = array("travel", "car", "dog", "word", "heart", "sound", "pretzel", "mus
 	"milk", "water", "snowman", "waterfall", "rainbow", "pencil", "ladder", "poster", "computer", "traffic", 
 	"galaxy", "avocado", "watermelon", "airplane", "giant", "telescope", "duck", "monitor", "toliet");
 
+# Array of special characters
+$specialChars = array("$", "#", "@", "!", "%", "&");
+
 # array for random password
 $pwd = [];
 
@@ -16,11 +19,35 @@ if (empty($_GET)) {
 }
 else {
 	$numwords = $_GET["pwdlength"];
+	if(!(is_numeric($numwords))) {
+		$error = "Invalid number. Enter number between 0-9.";
+	}
 }
 
-for($x = 1; $x <= $numwords; $x++) {
-	## Choose random word
-	$index = array_rand($words);
-	$pwd[$x] = $words[$index];
+
+
+# Check if user wants number in password, default is no (num = 0)
+if (empty($_GET["number"])) {
+	$num = 0;
 }
+else {
+	$num = rand(0,9);
+}
+
+
+# check if user wants a special character in password, default is no (spec = 0)
+if (isset($_GET["special"])) {
+	$specindex = array_rand($specialChars);
+	$spec = $specialChars[$specindex];
+}
+
+
+
+## choose random word
+
+for($x = 1; $x <= $numwords; $x++) {
+        $index = array_rand($words);
+        $pwd[$x] = $words[$index];
+}
+
 ?>
