42b0ffeb782440630c0550690c7e2048e67debc8
Allyson Bieryla <abieryla@cfa.harvard.edu>
2016-02-17 14:28:28 -0500

    Password generator working with set number of words - needs style and user input

--- index.php
+++ index.php
@@ -16,11 +16,28 @@ ini_set('display_errors', 1);
 
 <body>
 	<h1> Password Generator </h1>
+
+	<!-- Form that user will use to input password settings -->
 	<form action='index.php' method='GET'>
 		Enter number of words in password (0-9): 
 		<input type='text' name='numWords' size="5" maxlength="1"><br>
+		
+		Add a number? 
+		<input type='checkbox' name='option' value='number'><br>
+		Add a special character?
+		<input type='checkbox' name='option' value='special'><br>
+
 		<input type='submit' value='Generate a password!'>
 	</form>
 
+	<!-- Generate Password  using logic.php and print to screen -->
+	<?php $length = count($pwd); ?>
+	<?php for($x = 1; $x <= $length; $x++): ?>
+		<?php if($x < $length): ?>
+			<?php echo $pwd[$x]; ?> -
+		<?php else: ?>
+			<?php echo $pwd[$x]; ?>
+		<?php endif; ?>
+	<?php endfor; ?>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,11 +1,19 @@
 <?php
 
+$_GET;
+
+# Array of words for password
 $words = array("travel", "car", "dog", "word", "heart", "sound", "pretzel", "music", "guitar", "windmill", "chicken",
 	"milk", "water", "snowman", "waterfall", "rainbow", "pencil", "ladder", "poster", "computer", "traffic", 
 	"galaxy", "avocado", "watermelon", "airplane", "giant", "telescope", "duck", "monitor", "toliet");
 
-#var_dump($words);
+# array for random password
+$pwd = [];
+$numwords = '3';
 
-$pwd = array_rand($words);
-var_dump($pwd);
+for($x = 1; $x <= $numwords; $x++) {
+	## Choose random word
+	$index = array_rand($words);
+	$pwd[$x] = $words[$index];
+}
 ?>
